8ba4b8ba0f9d0600e3e39abfb3abab40eb6df8d2
nzkopraq <murthar@abv.bg>
2014-11-21 09:13:00 +0200

    commit1

---
+++ Source.cpp
@@ -0,0 +1,118 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout <<"Hello World!";
+	system("pause");
+	return 0;
+}
+*/
+
+
+/*
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int age = 10;
+	cout <<"Hi Im Peter and i " << age << " am years old";
+	system("pause");
+	return 0;
+}
+*/
+
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	bool loveProgramming = false;
+	bool loveGaming = true;
+	bool cutforbieber = false;
+	bool abdulahbar = false;
+	bool loveLudogorets = true;
+	cout <<"loveProgramming " <<loveProgramming <<endl <<"loveGaming " <<loveGaming <<endl <<"cutforbieber " <<cutforbieber <<endl <<"abdulahbar " <<abdulahbar <<endl <<"loveLudogorets " <<loveLudogorets <<endl;
+	system("pause");
+}
+*/
+
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int lubimochislo;
+
+	cout <<"Kakvo ti e lubimoto chislo?" <<endl;
+	cin >> lubimochislo;
+	cout <<"Ax, "<<lubimochislo<<"e i moeto lubimo chislo!" <<endl;
+	system("pause");
+	return 0;
+}
+*/
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int shirochina;
+	int visochina;
+	int perimetar;
+	cin >> shirochina;
+	cin >> visochina;
+	perimetar=(shirochina+visochina)*2;
+	cout <<"Perimatara na kvadrata e "<<perimetar<<" .";
+	system("pause");
+	return 0;
+}
+*/
+
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int price;
+	int VAT;
+	int number;
+	cout <<"Vuvedete price stoka shte zakupite";
+	cin >>price;
+	cout <<"Vuvedete number ot tazi stoka shte zakupite";
+	cin >>number;
+	VAT=(price*number)*0.2;
+	cout <<"Taksata Vi e "<<VAT<<" ";
+	system("pause");
+	return 0;
+}
+*/
+
+/*
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+int main()
+{
+	int Acoord;
+	int A1coord;
+	int B1coord;
+	int Bcoord;
+	int Duljina;
+	cout <<"Vuvdete koordinati na A";
+	cin >>Acoord>>A1coord>>Bcoord>>B1coord;
+	Duljina=sqrt(pow(A1coord-Acoord,2)+pow(B1coord-Bcoord,2));
+	cout <<"Duljinata mejdu A i B e "<<Duljina<<" ";
+	system("pause");
+	return 0;
+}
+*/
+
